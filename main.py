edce3261d8e674c29381b7b8bf678ac7201a1fc7
ujangkasep2504 <kerjabareng1111@gmail.com>
2025-10-12 10:30:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,39 @@ load_dotenv()
 import sys
 from app.menus.util import clear_screen, pause
 from app.client.engsel import *
+from app.client.engsel2 import get_tiering_info
+from app.menus.payment import show_transaction_history
 from app.service.auth import AuthInstance
 from app.menus.bookmark import show_bookmark_menu
 from app.menus.account import show_account_menu
 from app.menus.package import fetch_my_packages, get_packages_by_family
-from app.menus.hot import show_hot_menu
+from app.menus.hot import show_hot_menu, show_hot_menu2
+from app.service.sentry import enter_sentry_mode
+from app.menus.purchase import purchase_by_family
 
-def show_main_menu(number, balance, balance_expired_at):
+def show_main_menu(profile):
     clear_screen()
-    phone_number = number
-    remaining_balance = balance
-    expired_at = balance_expired_at
-    expired_at_dt = datetime.fromtimestamp(expired_at).strftime("%Y-%m-%d %H:%M:%S")
+    expired_at_dt = datetime.fromtimestamp(profile["balance_expired_at"]).strftime("%Y-%m-%d %H:%M:%S")
     
-    print("--------------------------")
+    print("-------------------------------------------------------")
     print("Informasi Akun")
-    print(f"Nomor: {phone_number}")
-    print(f"Pulsa: Rp {remaining_balance}")
+    print(f"Nomor: {profile['number']}")
+    print(f"Type: {profile['subscription_type']}({profile['subscriber_id']})")
+    print(f"Pulsa: Rp {profile['balance']}")
+    print(f"{profile['point_info']}")
     print(f"Masa aktif: {expired_at_dt}")
-    print("--------------------------")
+    print("-------------------------------------------------------")
     print("Menu:")
     print("1. Login/Ganti akun")
     print("2. Lihat Paket Saya")
     print("3. Beli Paket 🔥 HOT 🔥")
-    print("4. Beli Paket Berdasarkan Family Code")
-    print("5. Beli Paket Berdasarkan Family Code (Enterprise)")
+    print("4. Beli Paket 🔥 HOT-2 🔥")
+    print("5. Beli Paket Berdasarkan Family Code")
+    print("6. Riwayat Transaksi")
+    print("7. [Test] Purchase all packages in family code")
     print("00. Bookmark Paket")
     print("99. Tutup aplikasi")
-    print("--------------------------")
+    print("-------------------------------------------------------")
 
 show_menu = True
 def main():
@@ -45,8 +50,29 @@ def main():
             balance = get_balance(AuthInstance.api_key, active_user["tokens"]["id_token"])
             balance_remaining = balance.get("remaining")
             balance_expired_at = balance.get("expired_at")
+            
+            profile_data = get_profile(AuthInstance.api_key, active_user["tokens"]["access_token"], active_user["tokens"]["id_token"])
+            sub_id = profile_data["profile"]["subscriber_id"]
+            sub_type = profile_data["profile"]["subscription_type"]
+            
+            point_info = "Points: N/A | Tier: N/A"
+            
+            if sub_type == "PREPAID":
+                tiering_data = get_tiering_info(AuthInstance.api_key, active_user["tokens"])
+                tier = tiering_data.get("tier", 0)
+                current_point = tiering_data.get("current_point", 0)
+                point_info = f"Points: {current_point} | Tier: {tier}"
+            
+            profile = {
+                "number": active_user["number"],
+                "subscriber_id": sub_id,
+                "subscription_type": sub_type,
+                "balance": balance_remaining,
+                "balance_expired_at": balance_expired_at,
+                "point_info": point_info
+            }
 
-            show_main_menu(active_user["number"], balance_remaining, balance_expired_at)
+            show_main_menu(profile)
 
             choice = input("Pilih menu: ")
             if choice == "1":
@@ -62,32 +88,37 @@ def main():
             elif choice == "3":
                 show_hot_menu()
             elif choice == "4":
-                family_code = input("Enter family code (or '99' to cancel): ")
-                if family_code == "99":
-                    continue
-                get_packages_by_family(family_code)
+                show_hot_menu2()
             elif choice == "5":
                 family_code = input("Enter family code (or '99' to cancel): ")
                 if family_code == "99":
                     continue
-                get_packages_by_family(family_code, is_enterprise=True)
+                get_packages_by_family(family_code)
+            elif choice == "6":
+                show_transaction_history(AuthInstance.api_key, active_user["tokens"])
+            elif choice == "7":
+                family_code = input("Enter family code (or '99' to cancel): ")
+                if family_code == "99":
+                    continue
+                use_decoy = input("Use decoy package? (y/n): ").lower() == 'y'
+                pause_on_success = input("Pause on each successful purchase? (y/n): ").lower() == 'y'
+                purchase_by_family(family_code, use_decoy, pause_on_success)
             elif choice == "00":
                 show_bookmark_menu()
             elif choice == "99":
                 print("Exiting the application.")
                 sys.exit(0)
-            elif choice == "9":
-                # Playground
+            elif choice == "t":
+                res = get_package(
+                    AuthInstance.api_key,
+                    active_user["tokens"],
+                    ""
+                )
+                print(json.dumps(res, indent=2))
+                input("Press Enter to continue...")
                 pass
-                # data = get_package(
-                #     AuthInstance.api_key,
-                #     active_user["tokens"],
-                #     "U0NfX8A08oQLUQuLplGhfT_FXQokJ9GFF9kAKRiV5trm6BfbRoxrsizKkWIVNxM0az6lroT92FYXnWmTXRXZOl1Meg",
-                #     ""
-                #     ""
-                #     )
-                # print(json.dumps(data, indent=2))
-                # pause()
+            elif choice == "s":
+                enter_sentry_mode()
             else:
                 print("Invalid choice. Please try again.")
                 pause()
@@ -104,6 +135,5 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("\nExiting the application.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")        
